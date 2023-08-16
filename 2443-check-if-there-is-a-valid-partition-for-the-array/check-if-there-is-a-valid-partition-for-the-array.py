6889c5d7506bf9c11e381f50862a853cf151ce55
univac <68467947+univac-os@users.noreply.github.com>
2023-08-16 11:50:37 +0530

    Time: 845 ms (77.25%) | Memory: 30.4 MB (63.76%) - LeetSync

--- 2443-check-if-there-is-a-valid-partition-for-the-array/check-if-there-is-a-valid-partition-for-the-array.py
+++ 2443-check-if-there-is-a-valid-partition-for-the-array/check-if-there-is-a-valid-partition-for-the-array.py
@@ -1,8 +1,20 @@
 class Solution:
     def validPartition(self, nums: List[int]) -> bool:
         #edit distance
+        #memory optimized we need only 3 value to check bottom up approach
+        two=nums[-1]==nums[-2] #base case
+        if len(nums)==2:
+            return two
+        three=(nums[-1]==nums[-2]==nums[-3]) or (nums[-3]+1==nums[-2]==nums[-1]-1)
+        dp=[three,two,False]#inital value i+2 i+3 base
+        for i in range(len(nums) -4,-1,-1):
+            curr=(nums[i]==nums[i+1] )and dp[1]
+            curr=curr or ((nums[i]==nums[i+1]==nums[i+2]) or (nums[i]+1==nums[i+1]==nums[i+2]-1) ) and dp[2]
+            dp=[curr,dp[0],dp[1]]
+        
+        return dp[0]
         dp={}
-        def dfs(i):
+        def dfs(i): #O(n)
             if i==len(nums):
                 return True
             if i in dp:
