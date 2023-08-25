ec510b74e680a06c542f97e3e42f78feb2590260
univac <68467947+univac-os@users.noreply.github.com>
2023-08-25 11:09:01 +0530

    Time: 77 ms (26.88%) | Memory: 44.4 MB (73.92%) - LeetSync

---
+++ 97-interleaving-string/interleaving-string.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @param {string} s3
+ * @return {boolean}
+ */
+ 
+var isInterleave = function(s1, s2, s3) {
+    if(s1.length +s2.length!== s3.length){
+        return false;
+    }
+    const dp=new Array(s1.length+1).fill(false).map(()=>new Array(s2.length+1).fill(false));
+    dp[s1.length][s2.length]=true;
+
+    for (let i=s1.length;i>=0;i--){
+        for (let j=s2.length;j>=0;j--){
+            if (i<s1.length &&s1[i]===s3[i+j] && dp[i+1][j]){
+                dp[i][j]=true;
+            }
+            if(j<s2.length &&s2[j]===s3[i+j] && dp[i][j+1]){
+                dp[i][j]=true;
+            }
+        }
+    }
+    return dp[0][0]
+    
+};
