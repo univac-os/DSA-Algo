f5aae2b65b4539a7a46285e5b0028c2e4f7735e6
univac <68467947+univac-os@users.noreply.github.com>
2023-09-03 12:50:48 +0530

    Time: 44 ms (42.03%) | Memory: 16.7 MB (6.76%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,5 +1,20 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
+        dp={}
+        def dfs(i,j):
+            if i==0 and j==0:
+                return 1
+            if (i,j) in dp:
+                return dp[(i,j)]
+            if i<0 or j<0:
+                return 0
+            up=dfs(i-1,j)
+            left=dfs(i,j-1)
+            dp[(i,j)]=up+left
+            return dp[(i,j)]
+
+        
+        return dfs(m-1,n-1)
         dp=[[1]*n for _ in range(m)]
         for r in range(1,m):
             for c in range(1,n):
