656d0ec4b51143532031d601558e01bf07120927
univac <68467947+univac-os@users.noreply.github.com>
2023-09-04 10:46:48 +0530

    Time: 49 ms (96.02%) | Memory: 20.5 MB (48.63%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.py
+++ 141-linked-list-cycle/linked-list-cycle.py
@@ -9,7 +9,7 @@ class Solution:
         if head==None or head.next==None:
             return False
         slow=fast=head
-        while fast.next and fast.next.next :
+        while fast and fast.next :
             slow=slow.next
             fast=fast.next.next
             if slow==fast : return True
