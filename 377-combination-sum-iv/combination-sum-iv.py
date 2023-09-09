54f9d9be72f4e41e4b947b7166692d220a09bdbb
univac <68467947+univac-os@users.noreply.github.com>
2023-09-09 12:17:43 +0530

    Time: 54 ms (28.48%) | Memory: 16.6 MB (21.45%) - LeetSync

---
+++ 377-combination-sum-iv/combination-sum-iv.py
@@ -0,0 +1,19 @@
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        #for loop to check with every number
+        dp={}
+        def dfs(t):
+            if t==0:
+                return 1
+            if t<0:
+                return 0
+            if t in dp:
+                return dp[t]
+            res=0
+            for n in nums:
+                res+=dfs(t-n)
+            dp[t]=res
+
+            return dp[t]
+        
+        return dfs(target)
