d3d1fd57cdc051adefcd8099d92c28eabb64cc06
univac <68467947+univac-os@users.noreply.github.com>
2023-09-09 12:19:43 +0530

    Time: 46 ms (65.51%) | Memory: 16.3 MB (61.53%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.py
+++ 377-combination-sum-iv/combination-sum-iv.py
@@ -1,6 +1,15 @@
 class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
         #for loop to check with every number
+        #bottom up
+        dp={0:1} #base case
+        for total in range(1,target+1):
+            dp[total]=0
+            for n in nums:
+                dp[total]+=dp.get(total-n,0)
+        
+        return dp[total]
+
         dp={}
         def dfs(t):
             if t==0:
@@ -17,3 +26,4 @@ class Solution:
             return dp[t]
         
         return dfs(target)
+
