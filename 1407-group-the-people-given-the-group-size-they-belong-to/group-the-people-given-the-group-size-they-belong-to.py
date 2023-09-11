67de4978dd67d35899691f2caded7a4127da7627
univac <68467947+univac-os@users.noreply.github.com>
2023-09-11 11:46:52 +0530

    Time: 77 ms (52.01%) | Memory: 16.4 MB (64.84%) - LeetSync

--- 1407-group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.py
+++ 1407-group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.py
@@ -1,5 +1,25 @@
 class Solution:
     def groupThePeople(self, groupSizes: List[int]) -> List[List[int]]:
+        #heap based on group size add number 
+        minH=[]
+        res=[]
+        for idx,size in enumerate(groupSizes):
+            heapq.heappush(minH,(size,idx)) #(3,0) (3,1),(1,5)..
+        
+        #now we need to create new list for same group size
+        count=0
+        while minH:
+            size,idx=heapq.heappop(minH)
+            if count==0:
+                #1st occurance
+                res.append([])
+                count=size
+            res[-1].append(idx)
+            count-=1
+        
+        return res
+        
+        
         #map of group and number 
         #GREEDY
         groups={}
