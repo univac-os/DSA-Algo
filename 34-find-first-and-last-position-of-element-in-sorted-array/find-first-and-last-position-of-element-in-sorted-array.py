54da787a358c58304b5a027b977ac3e8c52b7bac
univac <68467947+univac-os@users.noreply.github.com>
2023-10-11 13:45:48 +0530

    Time: 89 ms (24.59%) | Memory: 17.9 MB (11.22%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,5 +1,54 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
+    #1. we can use same find left most value and for right target is +1
+        def findFirst(nums, target):
+            left, right = 0, len(nums) - 1
+            while left <= right:
+                mid = (left+right) // 2
+                if nums[mid] == target:#check mid is at start or left side is same
+                    if mid == 0 or nums[mid - 1] != target:
+                        return mid
+                    else:
+                        right = mid - 1
+                elif nums[mid] < target:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            return -1
+        
+        def findLast(nums, target):
+            left, right = 0, len(nums) - 1
+            while left <= right:
+                mid = (left+right) // 2
+                if nums[mid] == target:#check mid is last or mid+1 value is same 
+                    if mid == len(nums) - 1 or nums[mid + 1] != target:
+                        return mid
+                    else:
+                        left = mid + 1
+                elif nums[mid] < target:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            return -1
+        
+        first = findFirst(nums, target)
+        last = findLast(nums, target)
+        return [first, last]
+        
+        def RightBinSearch(nums,target):
+            l,r=0,len(nums)-1
+            while l<r:
+                m=(l+r)//2
+                if nums[m]>target:
+                    r=m-1
+                else: #nums[m]<=target
+                    l=m
+            return l
+        left=LeftBinSearch(nums,target)
+        right=RightBinSearch(nums,target)
+        return [left,right]
+    
+    #2. O(logn +logn)
         left = self.binSearch(nums,target, True)
         right = self.binSearch(nums,target, False)
         return [left, right]
