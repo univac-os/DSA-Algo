0d0716c3bfda600112d4a07789bc422171145ccd
univac <68467947+univac-os@users.noreply.github.com>
2024-04-29 12:10:52 +0530

    Time: 566 ms (92.99%) | Memory: 31.1 MB (50.32%) - LeetSync

--- 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.py
+++ 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.py
@@ -1,21 +1,17 @@
 class Solution:
     def minOperations(self, nums: List[int], k: int) -> int:
-        final_xor = 0
-        # XOR of all integers in the array.
+        #check with example
+        #if odd 1 then we get 1
+        #we need to check on many 1 in final output
+        final_xor=0
         for n in nums:
-            final_xor = final_xor ^ n
-
-        count = 0
-        # Keep iterating until both k and final_xor becomes zero.
+            final_xor^=n
+        # return bin(final_xor^k).count('1')
+        count=0
         while k or final_xor:
-            # k % 2 returns the rightmost bit in k,
-            # final_xor % 2 returns the rightmost bit in final_xor.
-            # Increment counter, if the bits don't match.
-            if (k % 2) != (final_xor % 2):
-                count += 1
-
-            # Remove the last bit from both integers.
-            k //= 2
-            final_xor //= 2
-
+            if(k%2) !=(final_xor%2):
+                count+=1
+            k //=2
+            final_xor //=2
         return count
+        
