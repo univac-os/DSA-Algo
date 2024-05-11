eb6f4c92183e546b44db507dc5fc47a1533a30cc
univac <68467947+univac-os@users.noreply.github.com>
2024-05-11 18:05:30 +0530

    Time: 522 ms (64.58%) | Memory: 19.5 MB (31.64%) - LeetSync

--- 221-maximal-square/maximal-square.py
+++ 221-maximal-square/maximal-square.py
@@ -1,6 +1,24 @@
 class Solution:
     def maximalSquare(self, matrix: List[List[str]]) -> int:
         # looks dp as subproblem so check down,right,diagonal
+        # bottom up DP 
+        if not matrix:
+            return 0
+        
+        row,col=len(matrix),len(matrix[0])
+        dp=[[0]*(col+1) for _ in range(row+1)]
+        max_val=0
+        #base case not needed actually as we have taken care above col+1 and row+1 
+        for r in range(row-1,-1,-1):
+            for c in range(col-1,-1,-1):
+                if matrix[r][c]=="1":
+                    down=dp[r+1][c]
+                    right=dp[r][c+1]
+                    diag=dp[r+1][c+1]
+                    dp[r][c]=1+min(down,right,diag)
+                    max_val=max(max_val,dp[r][c])
+        return max_val**2
+        
         
         # memo O(m*n) space O(m*n) top down
         row,col=len(matrix),len(matrix[0])
