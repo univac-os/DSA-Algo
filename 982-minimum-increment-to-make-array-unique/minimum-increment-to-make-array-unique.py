b5e9f08b2dcea5be2174e1f62d0e2c4f28e96058
univac <68467947+univac-os@users.noreply.github.com>
2024-06-14 11:54:50 +0530

    Time: 658 ms (35.81%) | Memory: 30.3 MB (66.44%) - LeetSync

---
+++ 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minIncrementForUnique(self, nums: List[int]) -> int:
+        #we need unique so sort value and make the array increasing order
+        # 4 3 are 2 nums so here we need increase 3 to 5(diff of 4-3 +1)
+        nums.sort()
+        res=0
+        for i in range(1,len(nums)):
+            if nums[i-1]>=nums[i]:
+                res+=1+ nums[i-1]-nums[i]
+                nums[i]=nums[i-1]+1
+        return res
+        
