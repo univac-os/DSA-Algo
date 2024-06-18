8b5469a81c718fea2c5fe12eb376cc8969f24439
univac <68467947+univac-os@users.noreply.github.com>
2024-06-18 21:20:45 +0530

    Time: 312 ms (37.72%) | Memory: 19 MB (99.17%) - LeetSync

--- 853-most-profit-assigning-work/most-profit-assigning-work.py
+++ 853-most-profit-assigning-work/most-profit-assigning-work.py
@@ -4,14 +4,14 @@ class Solution:
         # sort worker and difficulty and get the value O(nlogn. +mlogm) -2 ptr
         #instead of sort worker ,sort difficulty and bineay search on difficulty array O(nlogn)
         #max heap and start from reverse of worker(sorted) and get the value O(nlogn+ mlogm)
-        Job=namedtuple('WorkJob',['difficulty','profit'])
+        Job=namedtuple('WorkJob',['diff','pro'])
         worker.sort()
         work_queue=deque(sorted(list(Job(d,p) for d,p in zip(difficulty,profit) ),key=lambda x:x[0] ) ) 
         res=0
         best=0
         for w in worker:
-            while work_queue and work_queue[0].difficulty<=w:
-                best=max(best,work_queue[0].profit)
+            while work_queue and work_queue[0].diff<=w:
+                best=max(best,work_queue[0].pro)
                 work_queue.popleft()
             res+=best
         return res
