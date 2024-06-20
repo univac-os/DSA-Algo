c6402801cf0012e98cc04d89aac30fcbc0a3310f
univac <68467947+univac-os@users.noreply.github.com>
2024-06-20 18:10:43 +0530

    Time: 766 ms (86.04%) | Memory: 30.2 MB (67.81%) - LeetSync

---
+++ 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.py
@@ -0,0 +1,25 @@
+class Solution:
+    def maxDistance(self, position: List[int], m: int) -> int:
+        #we need max from all mini 
+        #check distance and move binaray search
+        def canFind(req_dist):
+            balls=0
+            prev_idx=float('-inf')
+            for b in position:
+                if b- prev_idx>=req_dist:
+                    balls+=1
+                    prev_idx=b
+            return balls>=m
+
+        
+        position.sort()
+        l=1
+        r=position[-1]-position[0] #we will search from start to end 
+        while l<r:
+            mid=(l+r+1)//2
+            if canFind(mid):
+                #we got a value GREEDY we need maxi
+                l=mid
+            else:
+                r=mid-1
+        return l
