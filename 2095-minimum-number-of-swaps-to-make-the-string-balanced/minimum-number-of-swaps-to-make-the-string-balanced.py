71ef74429c509a6a291e583528085662389fde68
univac <68467947+univac-os@users.noreply.github.com>
2024-07-01 11:56:27 +0530

    Time: 463 ms (17.02%) | Memory: 27.9 MB (67.82%) - LeetSync

---
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minSwaps(self, s: str) -> int:
+        #we need pair [ ] but here we need min swap
+        #given that there will be n/2--> [ , n/2 ->],So if we need max[ at any point there will be ] and end to make a pair
+        close,maxClose=0,0
+        for n in s:
+            if n=='[':
+                close-=1
+            else:
+                close+=1
+            maxClose=max(maxClose,close)
+        #we got at some point we have max ] that need pair so we can use [ from end 
+        return (maxClose+1)//2
