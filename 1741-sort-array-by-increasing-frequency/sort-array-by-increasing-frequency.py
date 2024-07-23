44387ecc27ab6c95ace1849671fe35947332ead9
univac <68467947+univac-os@users.noreply.github.com>
2024-07-23 20:02:50 +0530

    Time: 50 ms (55.36%) | Memory: 16.6 MB (12.04%) - LeetSync

--- 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.py
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.py
@@ -1,6 +1,7 @@
 class Solution:
     def frequencySort(self, nums: List[int]) -> List[int]:
-        #custom sort 
+        #custom sort --check no of occurance and if both are same check which is larger value 
+        #to check larger value we use -n and give smallest -ve value 
         count=Counter(nums)
 
         nums.sort(key=lambda n:(count[n],-n))
