11a33ec21abb5cd2b3cc1fb1d662842df81aee89
univac <68467947+univac-os@users.noreply.github.com>
2024-08-03 13:34:33 +0530

    Time: 80 ms (23.31%) | Memory: 16.9 MB (22.18%) - LeetSync

--- 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.py
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.py
@@ -1,4 +1,15 @@
 class Solution:
     def canBeEqual(self, target: List[int], arr: List[int]) -> bool:
+        #another way is create hashmap add element from targert and delete the element of the arr
+        # from hashmap and check finally hashmap is empty O(n) 
+        count=defaultdict(int)
+        for n1,n2 in zip(target,arr):
+            count[n1]+=1
+            count[n2]-=1
+        for n in count:
+            if count[n]!=0:
+                return False
+        return True
+        
         #similar to isAnagram
         return Counter(target)==Counter(arr)
