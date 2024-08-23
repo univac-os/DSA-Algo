f2fbeed9f77662ac33e4b8088bf5b4c6169267ec
univac <68467947+univac-os@users.noreply.github.com>
2024-08-23 10:55:43 +0530

    Time: 139 ms (33.34%) | Memory: 27.7 MB (10.39%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.py
+++ 650-2-keys-keyboard/2-keys-keyboard.py
@@ -1,20 +1,21 @@
 class Solution:
     def minSteps(self, n: int) -> int:
+        #looks like pick no pick but here we can paste--> 1 operation or copy&paste --> 2 operation
+        #we need min so dp 
+        #we will have count of operation
         cache = {}
         
         def dfs(count, paste):
             if count == n:
                 return 0
             if count > n:
-                return float('inf')  # Use infinity to signify an invalid path
+                return 1000  # Use infinity to signify an invalid path
             
             if (count, paste) in cache:
-                return cache[(count, paste)]
-            
-            # Option 1: Paste the current clipboard content
+                return cache[(count, paste)]  
+            #Paste the current clipboard content
             paste_option = 1 + dfs(count + paste, paste)
-            
-            # Option 2: Copy all and then paste (2 operations: copy + paste)
+            #Copy all and then paste (2 operations: copy + paste)
             copy_paste_option = 2 + dfs(count + count, count)
             
             cache[(count, paste)] = min(paste_option, copy_paste_option)
@@ -22,6 +23,5 @@ class Solution:
         
         if n == 1:
             return 0
-        
         # Start with 1 character on the board and 1 character in the clipboard
         return 1 + dfs(1, 1)
