8bfd79a20c00f72bff4435873d8df2a3a982e76a
univac <68467947+univac-os@users.noreply.github.com>
2024-08-23 10:57:55 +0530

    Time: 214 ms (23.40%) | Memory: 16.5 MB (86.50%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.py
+++ 650-2-keys-keyboard/2-keys-keyboard.py
@@ -3,8 +3,18 @@ class Solution:
         #looks like pick no pick but here we can paste--> 1 operation or copy&paste --> 2 operation
         #we need min so dp 
         #we will have count of operation
+
+        #top-down we need to get the factor 
+        dp=[1000]*(n+1)
+        dp[1]=0
+        for i in range(1,n+1):
+            for j in range(1,1+ (i//2)):
+                if i%j==0:
+                    #its factor
+                    dp[i]=min(dp[i],dp[j]+i//j)
+        return dp[n] 
+
         cache = {}
-        
         def dfs(count, paste):
             if count == n:
                 return 0
