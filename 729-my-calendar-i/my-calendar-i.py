cf22c6c04a57e87e7ff7ce845f2ec5e581518cb7
univac <68467947+univac-os@users.noreply.github.com>
2024-09-26 17:16:59 +0530

    Time: 313 ms (37.53%) | Memory: 17.5 MB (24.81%) - LeetSync

--- 729-my-calendar-i/my-calendar-i.py
+++ 729-my-calendar-i/my-calendar-i.py
@@ -1,4 +1,41 @@
-#Optimal solution is to sort,but each time after append and sort similar to heap
+#Optimal solution is to sort,but each time after append and sort similar to heap O(nlogn)
+#worst it can go to O(n2)
+#BETTER --- similar to heapq ,here we can use BST based on start and end we add node to left or right O(n)
+class BST:
+    def __init__(self,start,end):
+        self.left,self.right=None,None
+        self.start,self.end=start,end
+    
+    def insert(self,start,end):
+        curr=self
+        while True:
+            if start >=curr.end:
+                #add to right ,check right side
+                if curr.right==None:
+                    curr.right=BST(start,end)
+                    return True
+                curr=curr.right
+            elif end <=curr.start:
+                #add to left ,check left side
+                if curr.left==None:
+                    curr.left=BST(start,end)
+                    return True
+                curr=curr.left
+            else:
+                #overlapp
+                return False
+
+
+class MyCalendar:
+    def __init__(self):
+        self.root=None
+
+    def book(self,start,end):
+        if not self.root:
+            self.root=BST(start,end)
+            return True
+        return self.root.insert(start,end)
+
 class MyCalendar:
     #brute force check with each one whether it overlapping or not O(n2)
     def __init__(self):
