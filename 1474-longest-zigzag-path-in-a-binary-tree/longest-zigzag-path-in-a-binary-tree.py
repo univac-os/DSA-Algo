e9a9cd17246658348d25ba10fc17b4858924b4ee
univac <68467947+univac-os@users.noreply.github.com>
2024-10-07 15:56:32 +0530

    Time: 214 ms (33.43%) | Memory: 30.4 MB (45.65%) - LeetSync

--- 1474-longest-zigzag-path-in-a-binary-tree/longest-zigzag-path-in-a-binary-tree.py
+++ 1474-longest-zigzag-path-in-a-binary-tree/longest-zigzag-path-in-a-binary-tree.py
@@ -9,13 +9,13 @@ class Solution:
         #dp dfs  true-left false -right ki po
         def dfs(node,dir,count):
             if node==None:
-                return count-1
+                return count
             if dir:
-                left=dfs(node.left,False,count+1)
-                right=dfs(node.right,True,1)# start from here
+                left=dfs(node.left,False,count+1) if node.left else count
+                right=dfs(node.right,True,1) if node.right else count# start from here
             else:
-                right=dfs(node.right,True,count+1)
-                left=dfs(node.left,False,1)#start from here
+                right=dfs(node.right,True,count+1) if node.right else count
+                left=dfs(node.left,False,1) if node.left else count#start from here
             return max(left,right)
         
         return max(dfs(root,True,0),dfs(root,False,0))
