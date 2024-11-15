bba463e1388a7003c1b2675459fbb2b77587921e
univac <68467947+univac-os@users.noreply.github.com>
2024-11-15 17:51:13 +0530

    Time: 7 ms (100.00%) | Memory: 32 MB (6.23%) - LeetSync

--- 1679-shortest-subarray-to-be-removed-to-make-array-sorted/shortest-subarray-to-be-removed-to-make-array-sorted.py
+++ 1679-shortest-subarray-to-be-removed-to-make-array-sorted/shortest-subarray-to-be-removed-to-make-array-sorted.py
@@ -6,18 +6,20 @@ class Solution:
         now from front we will check with this backside 
         so we will reduce backside sorted protion with frontside so that middle part is gone O(n)
         """
-        #increasing backside
+        #get increasing backside
         N=len(arr)
         r=N-1
         while r>0 and arr[r-1]<=arr[r]:
             r-=1
-        res=r #get the ptr of start of backside increasing order 
+        res=r #get starting index of backside
         l=0
-        #check with frontside and reduce backside sub array
+        #Now check frontside and backside and reduce backside subarray so that we will get middle part
         while l<r:
+            #to remove middle part
             while r<N and arr[l]>arr[r]:
                 r+=1
             res=min(res,r-l-1)
+            #check frontside
             if arr[l]>arr[l+1]:
                 break
             l+=1
