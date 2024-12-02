5960fbbac29862f4efa30f2c7154e009f3e6a2e2
univac <68467947+univac-os@users.noreply.github.com>
2024-12-02 17:52:51 +0530

    Time: 11 ms (75.24%) | Memory: 19 MB (5.52%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,36 +1,30 @@
 class Solution:
-    def trap(self, height):
-        n = len(height)
-        if n == 0:
-            return 0
-
-        stack = []
-        trapped_water = 0
-
+    def trap(self, height:List[int])->int:
+        """
+            1.prefix_max start from front and suffix_max from back then 
+                min(prefix_max[i]-suffix_max[i])-arr[i] --> O(n) and space O(n)
+            2. find leftmax and rightmax 2ptr aproach --> O(n) and space O(1)
+            3.llooking at this pattern at any index we want the wall on both side ,
+            then calculate the area similar to max area in histogram
+            from that index go till you find next greater element,after finding it we will go back and   
+            find the area -->O(n) and space O(n)
+        """
+        n=len(height)
+        if n==0:return 0
+        stack=[] #(idx)
+        res=0
+        
         for i in range(n):
-            # Process the "valley" when current height is greater than the top of the stack
-            while stack and height[i] > height[stack[-1]]:
-                top = stack.pop()
-
+            while stack and height[i]>height[stack[-1]]:
+                #we found next greater element
+                right_wall=stack.pop()
                 if not stack:
-                    break  # No left boundary, can't trap water
+                    break # no left wall
+                distance=i-stack[-1]-1
+                ht=min(height[i],height[stack[-1]])-height[right_wall]
+                res+=distance*ht
+            
 
-                # Calculate the distance between the current index and the stack top (left boundary)
-                distance = i - stack[-1] - 1
-
-                # Calculate the bounded height for water trapping
-                bounded_height = min(height[i], height[stack[-1]]) - height[top]
-
-                # Add the trapped water for this valley
-                trapped_water += distance * bounded_height
-
-            # Push the current index onto the stack
             stack.append(i)
+        return res
 
-        return trapped_water
-
-
-# Test case
-height = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
-solution = Solution()
-print(solution.trap(height))  # Output: 6
