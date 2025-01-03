79c48a8ae9d41301be48a9cbcf0977f4334866b9
univac <68467947+univac-os@users.noreply.github.com>
2025-01-03 18:13:14 +0530

    Time: 16 ms (78.98%) | Memory: 49.7 MB (47.98%) - LeetSync

--- 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.py
+++ 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.py
@@ -2,7 +2,8 @@ class Solution:
     def vowelStrings(self, words: List[str], queries: List[List[int]]) -> List[int]:
         #similar to prefix sum 
         # here we will get the sum of right and minus them 
-        vowel=('a','e','i','o','u')
+        # vowel=('a','e','i','o','u')
+        vowel=set('aeiou')
         pref=[0]*(len(words)+1) # have inital start point as we will remove queries
         prev=0
         for idx,w in enumerate(words):
