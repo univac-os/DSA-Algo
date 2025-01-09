88842208f24448d55ac310192592c5139f2ae288
univac <68467947+univac-os@users.noreply.github.com>
2025-01-09 12:32:21 +0530

    Time: 14 ms (87.69%) | Memory: 18 MB (15.29%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -3,22 +3,32 @@ class Solution:
         """
         sliding window but we need matches
         1. hashmap for s1 and s2 check the count of char in s1 same as s2 in that window O(26n)
-        2.Better same hashmap here we will have matches
+        2.Better use array of 26 and check the value
         """
         if len(s1) > len(s2):
             return False
 
-        
+        s1_count=[0]*26
+        window_count=[0]*26
 
-        # Count characters in s1
+        for i in range(len(s1)):
+            s1_count[ord(s1[i])-ord('a')]+=1
+
+        for i in range(len(s2)):
+            window_count[ord(s2[i])-ord('a')]+=1
+
+            if i >=len(s1):
+                #Move window
+                window_count[ord(s2[i -len(s1)])-ord('a')]-=1
+            if window_count==s1_count:
+                return True
+        return False
+
         s1_count = Counter(s1)
         window_count = Counter()
 
-        # Sliding window over s2
         for i in range(len(s2)):
-            # Add the current character to the window
             window_count[s2[i]] += 1
-
             # Remove the character that is left out of the window
             if i >= len(s1):
                 if window_count[s2[i - len(s1)]] == 1:
@@ -26,7 +36,6 @@ class Solution:
                 else:
                     window_count[s2[i - len(s1)]] -= 1
 
-            # Compare the two counters
             if window_count == s1_count:
                 return True
 
