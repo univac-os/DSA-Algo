41874ca238635eb5f35b99a7ed8defbf39e70539
univac <68467947+univac-os@users.noreply.github.com>
2025-01-23 18:26:34 +0530

    Time: 0 ms (100.00%) | Memory: 21.5 MB (20.89%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.py
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.py
@@ -29,3 +29,37 @@ class Solution:
                 #now remove this node
                 root.right=self.deleteNode(root.right,curr.val)
         return root
+    ###################
+
+    def findMaxLeft(self,root):
+        while root.right:
+            root=root.right
+        return root
+    
+    def helper(self,root):
+        #go left subtrees from there find max value[go right] and attach the inital right subtree to it
+        if root.left is None:
+            return root.right
+        elif root.right is None:
+            return root.left
+        
+        #both side we have node so recursion
+        rightChild=root.right
+        #find max val on left subtree
+        maxLeft=self.findMaxLeft(root.left)
+        maxLeft.right=rightChild
+
+        return root.left
+
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        if not root:
+            return None
+        
+        if key < root.val:
+            root.left = self.deleteNode(root.left, key)
+        elif key > root.val:
+            root.right = self.deleteNode(root.right, key)
+        else:
+            return self.helper(root)
+        
+        return root
