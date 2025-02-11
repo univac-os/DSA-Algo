4df0c58003d794edfc00ac2bf7d0f11c221c1fbd
univac <68467947+univac-os@users.noreply.github.com>
2025-02-11 17:23:04 +0530

    Time: 0 ms (100.00%) | Memory: 18.5 MB (36.83%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
@@ -8,8 +8,22 @@ class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         """
         BFS is better O(n)
+        with DFS go till end and backtrack
         """
         if not root:return []
+        res=[]
+        def dfs(root,level):
+            if not root:
+                return
+            if len(res)==level:#first time
+                res.append([])
+            res[level].append(root.val)
+            dfs(root.left,level+1)
+            dfs(root.right,level+1)
+        
+        dfs(root,0)
+        return res
+
         q=deque([root])
         res=[]
         while q:
