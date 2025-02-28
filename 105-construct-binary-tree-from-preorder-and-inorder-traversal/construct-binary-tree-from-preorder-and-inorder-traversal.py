29d77614d41273e748aa153e7843c9f6c6ab7e6e
univac <68467947+univac-os@users.noreply.github.com>
2025-03-01 00:44:44 +0530

    Time: 3 ms (90.08%) | Memory: 19.1 MB (91.83%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -7,15 +7,20 @@
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
         """
-        1st num in preorder is root,find this num in inord,now left side to this num are the node present on left side 
+        1st num in preorder(NLR) is root,find this num in inord,now left side to this num are the node present on left side 
         How many are there check from 0 to index of root in inorder and use this length to find in postorder form start AND remaining are on right side
         """
-        if not preorder or not inorder:
-            return None
-        #find root
-        root=TreeNode(preorder[0])
-        root_idx=inorder.index(preorder[0])
-        #build left and right
-        root.left=self.buildTree(preorder[1:root_idx+1],inorder[:root_idx])
-        root.right=self.buildTree(preorder[root_idx+1:],inorder[root_idx+1:])
-        return root
+        inorder_Idx={v:i for i,v in enumerate(inorder)}
+        def helper(l,r):
+            if l>r:
+                return None
+            #find root in inorder
+            root=TreeNode(preorder.pop(0))#pop at idx=0
+            root_idx=inorder_Idx[root.val]
+            #build left and right
+            root.left=helper(l,root_idx-1)
+            root.right=helper(root_idx+1,r)
+            return root
+        
+
+        return helper(0,len(inorder)-1)
