c6718d0ca5dc7f8139fac22ce4fa1f9dfe4d0b96
univac <68467947+univac-os@users.noreply.github.com>
2025-03-15 20:27:26 +0530

    Time: 0 ms (100.00%) | Memory: 21.4 MB (40.39%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -2,7 +2,19 @@ class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         """
         we will have min heap of len k only ,whenever required we remove low freq value from heap
+        BETTER is use bucket sort have counter and an array of freq where we will add num to taht freq num
         """
+        count=Counter(nums)
+        freq=defaultdict(list)
+        for num,cnt in count.items():
+            freq[cnt].append(num)
+        res=[]
+        freq = dict(sorted(freq.items(),reverse=True)) #gives turple by default
+        for cnt,lt in freq.items():
+            for l in lt:
+                if len(res)<k:
+                    res.append(l)
+        return res
         count=Counter(nums) #num:count
         min_h=[]
 
