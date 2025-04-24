dc9a270853f29d84cb057893b4bd17ca95e78db1
univac <68467947+univac-os@users.noreply.github.com>
2025-04-24 18:08:26 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (14.60%) - LeetSync

--- 3154-maximum-value-of-an-ordered-triplet-i/maximum-value-of-an-ordered-triplet-i.py
+++ 3154-maximum-value-of-an-ordered-triplet-i/maximum-value-of-an-ordered-triplet-i.py
@@ -5,14 +5,15 @@ class Solution:
         so we want i--> maxi j-->mini k-->maxi
         instead of O(n3)we can go through nums and if we find any max value till now we go and find j and k 
         """
-        res=0
-        n=len(nums)
-        left=nums[0]
-        middle=nums[1]
-        for j in range(1,n):
-            if nums[j]>left:
-                left=nums[j]
-            for k in range(j+1,n):
-                res=max(res,(left -nums[j])*nums[k])
-        return res
+        max_i_minus_j = float('-inf')
+        result = 0
+        max_i = nums[0]
+
+        for j in range(1, len(nums) - 1):
+            max_i_minus_j = max(max_i_minus_j, max_i - nums[j])
+            max_i = max(max_i, nums[j])
+            result = max(result, max_i_minus_j * nums[j + 1])
+
+        return result
+
 
