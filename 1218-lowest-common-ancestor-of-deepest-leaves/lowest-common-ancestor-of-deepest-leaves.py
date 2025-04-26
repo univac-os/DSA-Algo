bc6d204926eedfd87ab794ab3547ecfe05622b80
univac <68467947+univac-os@users.noreply.github.com>
2025-04-26 20:04:16 +0530

    Time: 0 ms (100.00%) | Memory: 18.5 MB (5.35%) - LeetSync

--- 1218-lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.py
+++ 1218-lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.py
@@ -6,27 +6,47 @@
 #         self.right = right
 class Solution:
     def lcaDeepestLeaves(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        if not root:
-            return None
-        
-        queue = deque([root])
-        while queue:
-            level = list(queue)
-            for _ in range(len(queue)):
-                node = queue.popleft()
+        """
+        BFS find the children and take first and last children and Use LCA O(n)
+        can we do using DFS only in LCA code we can find height as well as we are going down to find the children node
+        but if i am doing from top how will know which is children node?--we can use height as parameter to check whether we can find children in going the path
+        """
+        if not root:return None
+        def dfs(node):
+            if not node:
+                return node,0
+            left_node,left_ht=dfs(node.left)
+            right_node,right_ht=dfs(node.right)
+            if left_ht==right_ht:
+                #so children on both side
+                return node,1+left_ht #add that node level as well
+            elif left_ht>right_ht:
+                #left side children are present
+                return left_node,1+left_ht
+            else:
+                #right side more
+                return right_node,1+right_ht
+            
+        node,_=dfs(root)
+        return node
+
+        q=deque([root])
+        while q:
+            level=list(q)
+            for _ in range(len(level)):
+                node=q.popleft()
                 if node.left:
-                    queue.append(node.left)
+                    q.append(node.left)
                 if node.right:
-                    queue.append(node.right)
-        print(level)
-        def lca(root, p, q):
-            if not root or root == p or root == q:
+                    q.append(node.right)
+
+        def LCA(root,p,q):
+            if not root or root==p or root==q:
                 return root
-            left = lca(root.left, p, q)
-            right = lca(root.right, p, q)
+            left=LCA(root.left,p,q)
+            right=LCA(root.right,p,q)
             if left and right:
                 return root
             return left if left else right
-        
-        # Just use first and last nodes
-        return lca(root, level[0], level[-1])
+
+        return LCA(root,level[0],level[-1])
