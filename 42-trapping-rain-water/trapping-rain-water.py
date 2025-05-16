2462d11ca9b87fcaae96d827e5ba3ec583af4bb5
univac <68467947+univac-os@users.noreply.github.com>
2025-05-17 00:58:04 +0530

    Time: 7 ms (89.13%) | Memory: 19.6 MB (8.50%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -4,7 +4,7 @@ class Solution:
             1.prefix_max start from front and suffix_max from back then 
                 min(prefix_max[i]-suffix_max[i])-arr[i] --> O(n) and space O(n)
             2. find leftmax and rightmax 2ptr aproach --> O(n) and space O(1)
-            3.llooking at this pattern at any index we want the wall on both side ,
+            3.looking at this pattern at any index we want the wall on both side ,
             then calculate the area similar to max area in histogram
             from that index go till you find next greater element,after finding it we will go back and   
             find the area -->O(n) and space O(n)
@@ -17,12 +17,14 @@ class Solution:
         for i in range(n):
             while stack and height[i]>height[stack[-1]]:
                 #we found next greater element
-                right_wall=stack.pop()
+                bottom=stack.pop()
+                right_wall=height[i]
                 if not stack:
                     break # no left wall
+                left_wall=height[stack[-1]]
                 distance=i-stack[-1]-1
-                ht=min(height[i],height[stack[-1]])-height[right_wall]
-                res+=distance*ht
+                ht=min(right_wall,left_wall)-height[bottom] #wall - bottom cell
+                res+=(distance*ht)
             
 
             stack.append(i)
