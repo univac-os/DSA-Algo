a794ebfbcef7f375122d2822c68433a5c9205b65
univac <68467947+univac-os@users.noreply.github.com>
2025-06-01 13:42:51 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (44.18%) - LeetSync

--- 770-couples-holding-hands/couples-holding-hands.py
+++ 770-couples-holding-hands/couples-holding-hands.py
@@ -1,22 +1,23 @@
 class Solution:
     def minSwapsCouples(self, row: List[int]) -> int:
-        n = len(row)
-        pos = {person: i for i, person in enumerate(row)}  # maps person -> index
-        swaps = 0
-
-        for i in range(0, n, 2):
-            first = row[i]
-            partner = first ^ 1  # get the expected partner
-            if row[i + 1] != partner:
-                partner_index = pos[partner]
-
-                # swap partner into the correct position (i+1)
-                row[i + 1], row[partner_index] = row[partner_index], row[i + 1]
-
-                # update positions after swap
-                pos[row[partner_index]] = partner_index
-                pos[row[i + 1]] = i + 1
-
-                swaps += 1
-
-        return swaps
+        """
+        we want pair in order (x,x+1) or(x+1,x) Linkedlist? No
+        we have couple same as index values so index transveral? gives cycle but no use
+        we need to swap the index position take 2 element at a time
+        HOW can i know pair value say x=2 them pair is 3 so here if x is even pair is x+1 else x-1
+        """
+        n=len(row)
+        pos={n:idx for idx,n in enumerate(row)} #hashmap
+        res=0
+        for i in range(0,n,2):
+            first=row[i]
+            partner=first+1 if first%2==0 else first-1
+            if partner !=row[i+1]:
+                #we need to swap
+                partner_idx=pos[partner]
+                #swap
+                row[i+1],row[partner_idx]=row[partner_idx],row[i+1]
+                #update pos 
+                pos[row[partner_idx]],pos[row[i+1]]=partner_idx,i+1
+                res+=1
+        return res
