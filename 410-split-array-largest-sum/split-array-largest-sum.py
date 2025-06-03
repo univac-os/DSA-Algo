376007252b1b4433a8cb63dcc5b61c03fb13fd05
univac <68467947+univac-os@users.noreply.github.com>
2025-06-04 00:38:29 +0530

    Time: 5 ms (19.42%) | Memory: 18.1 MB (22.19%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.py
+++ 410-split-array-largest-sum/split-array-largest-sum.py
@@ -4,7 +4,31 @@ class Solution:
         draw line between the nums so where 
         prefix sum and get min diff among all is less and take max value from it
         draw ,not draw each point 
+        better we know ans will be max(nums)..sum(nums) in between so find how many cuts can be made such that each group is less or equal to this BINARY SEARCH
         """
+        def check(m):
+            sub_arr=1
+            curr_sum=0
+            for n in nums:
+                if curr_sum+n>m:
+                    #new sub array need
+                    curr_sum=n
+                    sub_arr+=1
+                else:
+                    curr_sum+=n
+            return sub_arr<=k
+        l,r=max(nums),sum(nums)
+        res=0
+        while l<=r:
+            m=(l+r)//2
+            if check(m):
+                #greedy
+                res=m
+                r=m-1
+            else:
+                l=m+1
+        return res
+
         prefix_sum=[0]*(len(nums)+1)
         for i in range(len(nums)):
             prefix_sum[i+1]=prefix_sum[i]+nums[i]
@@ -21,7 +45,7 @@ class Solution:
                 right=dfs(j,cuts-1)
                 curr_max=max(left,right)
                 max_res=min(max_res,curr_max)
-                if left>max_res:break
+                if left>max_res:break #needed 
             cache[(i,cuts)]=max_res
             return max_res
                 
