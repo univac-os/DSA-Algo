0753f17973bfed3d9ecb29e1294e3fa665796188
univac <68467947+univac-os@users.noreply.github.com>
2025-07-05 17:50:53 +0530

    Time: 3 ms (47.72%) | Memory: 17.8 MB (75.29%) - LeetSync

--- 778-reorganize-string/reorganize-string.py
+++ 778-reorganize-string/reorganize-string.py
@@ -1,24 +1,23 @@
 class Solution:
     def reorganizeString(self, s: str) -> str:
-        count=Counter(s)#hashmap char-->count
-        mh=[[-cnt,char] for char,cnt in count.items()]
-        heapq.heapify(mh)#most freq char
-
+        """
+        so we need count of char and take the more no of char first and check with prev 
+        so we need keep the prev on hold till next operation
+        """
+        count=Counter(s)
+        max_h=[[-cnt,c] for c,cnt in count.items()]
+        heapq.heapify(max_h)
         prev=None
         res=""
-        while mh or prev:
-            if prev and not mh: # aaab --> aba prev=a mh is empty
-                return ""
-            
-            cnt,char=heapq.heappop(mh)
-            cnt+=1 #decrease by 1
+        while max_h or prev:
+            if prev and not max_h:return "" #there is prev value and no value to keep next to it
+            cnt,char=heapq.heappop(max_h)
+            cnt+=1 #decrease count
             res+=char
-
-            if prev:#prev is present add to heap for next loop
-                heapq.heappush(mh,prev)
+            #keep hold the this char to next iteraton but before this check with prev present or we can take in next loop
+            if prev:
+                heapq.heappush(max_h,prev)
                 prev=None
-
-            if cnt!=0:#char occurance present
-                prev=[cnt,char] # this should be done at last
-
+            if cnt!=0:# so this char is prev now
+                prev=[cnt,char]
         return res
