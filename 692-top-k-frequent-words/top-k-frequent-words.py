4c2f545cdea552e48d187737065b6bc5c0fb24fd
univac <68467947+univac-os@users.noreply.github.com>
2025-08-17 12:55:41 +0530

    Time: 2 ms (58.58%) | Memory: 17.7 MB (88.83%) - LeetSync

---
+++ 692-top-k-frequent-words/top-k-frequent-words.py
@@ -0,0 +1,30 @@
+class Compartor:
+    def __init__(self,cnt,word):
+        self.cnt=cnt
+        self.word=word
+    def __lt__(self,other):
+        if self.cnt==other.cnt:
+            return self.word>other.word #same count large lexi is smaller
+        return self.cnt<other.cnt
+
+
+class Solution:
+    
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        """
+        max heap of size k O(nlogk) or can we use dict and do it
+        
+        """
+        
+        min_h=[]
+        count=Counter(words)
+        for w,cnt in count.items():
+            heapq.heappush(min_h,Compartor(cnt,w))
+            if len(min_h)>k:
+                heapq.heappop(min_h)
+        result = []
+        while min_h:
+            element = heapq.heappop(min_h)
+            result.append(element.word)
+        return result[::-1] # this correct but check count same then sort with alphablet so wrong we need custom comparator
+
