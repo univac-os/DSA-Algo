a42bae3566a5a93b22370deed9ecb3b88d450f70
univac <68467947+univac-os@users.noreply.github.com>
2025-08-23 13:44:48 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (89.11%) - LeetSync

--- 68-text-justification/text-justification.py
+++ 68-text-justification/text-justification.py
@@ -1,34 +1,36 @@
 class Solution:
     def fullJustify(self, words: List[str], maxWidth: int) -> List[str]:
+        """
+        greedy 
+        word len add to curr line and reamaining space in between them equally 
+        if after even distribution also therre are left over then give it to left to right word O(N)
+        """
         res=[]
-        curr_line,length=[],0
-        i=0
-        while i<len(words):
-            #lines completes with words add spaces 
-            #present word lenght + 1 space b/w them + new word
-            if length+len(curr_line)+len(words[i])>maxWidth:
-                #we cant add this word to line so add spaces b/w the already words
-                #and also greedy way as mentioned
-                extra_space=maxWidth-length
-                spaces=extra_space//max(1,len(curr_line)-1) #word present in line
-                reminder=extra_space%max(1,len(curr_line)-1) #greedy 
-                for j in range(max(1,len(curr_line)-1)):
-                    curr_line[j]+=" "*spaces
-                    if reminder:
-                        curr_line[j]+=" "
-                        reminder-=1
+        curr,curr_len=[],0
+        for w in words:
+            #check if word cant fit in curr line then add space and create a new line to it 
+            #include +1 space for each word mandatory len(curr) is for this take
+            if curr_len+len(curr)+len(w)>maxWidth:
+                # take extra spaces and distribute in between words
+                extra_space=maxWidth-curr_len
+                even_dist_space=extra_space//(max(1,len(curr)-1))
+                remain_dist_space=extra_space%(max(1,len(curr)-1))
+                for i in range(max(1,len(curr)-1)):
+                    curr[i]+=" "*even_dist_space
+                    if remain_dist_space:
+                        curr[i]+=" "
+                        remain_dist_space-=1
 
-                res.append("".join(curr_line)) # add to res
-                curr_line,length=[],0 # new line
-
-            #add word to  the line 
-            curr_line.append(words[i])
-            length+=len(words[i])
-            i+=1
-
-        #edge case when we are take last line and no words then add spaces after words
-        last_line=" ".join(curr_line)
-        tail_spaces=maxWidth-len(last_line)
-        last_line+= " "*tail_spaces
+                res.append("".join(curr))
+                curr,curr_len=[],0 #new curr line create
+            
+            #add word to line
+            curr.append(w)
+            curr_len+=len(w)
+        
+        #edge case last line only 1 word then add extra space to end
+        last_line=" ".join(curr)
+        tail_space=maxWidth-len(last_line)
+        last_line+=" "*tail_space
         res.append(last_line)
         return res
