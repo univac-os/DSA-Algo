21a8a20e28e02d27eb1ef7796f752dd85d138dc2
univac <68467947+univac-os@users.noreply.github.com>
2025-09-08 17:11:28 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (49.24%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -2,5 +2,8 @@ class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         """
         convert to str and then add +1 then convert to list
+        int(''.join(map(str,digits))) +1 ) join num to str 123 +1 
+        str(int(''.join(map(str,digits))) +1 ) convert 124 to str
+        map(int,str(124)) to 124 interger and finally list [1,2,4]
         """
-        return list(map(int,str(int(''.join(map(str,digits))) +1)))
+        return list(map(int,str(int(''.join(map(str,digits))) +1 )))
