02170db1ccf898893e58c57339119198f180a996
univac <68467947+univac-os@users.noreply.github.com>
2025-10-21 23:50:28 +0530

    Time: 319 ms (6.93%) | Memory: 34.3 MB (38.99%) - LeetSync

--- 1612-avoid-flood-in-the-city/avoid-flood-in-the-city.py
+++ 1612-avoid-flood-in-the-city/avoid-flood-in-the-city.py
@@ -6,21 +6,24 @@ class Solution:
         1 2 2 1 so rain falls on 1 then 2 and again 2 and 1 -so here if have 0 first dry 2 as it comes first then 1
         """
         n=len(rains)
+        st=SortedList() # zero-index in order based of index
+        mp={} # lake-num--index
         res=[1]*n
-        st=SortedList() # drying area in increasing order based on index
-        mp={} #lake-num-->index
-        for idx,r in enumerate(rains):
-            if r==0:
-                st.add(idx)
+        for i,r in enumerate(rains):
+            if r ==0:
+                #add to zero-index
+                st.add(i)
             else:
-                res[idx]=-1
-                #first time no problem if not then use drying area
+                #check this lake need to be dry before come here
+                res[i]=-1
                 if r in mp:
-                    dry_day=st.bisect(mp[r])
-                    if dry_day==len(st):
-                        #no found so flood
+                    #check whether the lake from prev --x--current has any 0 in between
+                    zero_between=st.bisect(mp[r])
+                    if zero_between==len(st):
+                        #not present so flood
                         return []
-                    res[st[dry_day]]=r
-                    st.discard(st[dry_day])
-                mp[r]=idx
+                    res[st[zero_between]]=r #dry that lake
+                    st.discard(st[zero_between])#remove from list
+                
+                mp[r]=i #add current day 
         return res
