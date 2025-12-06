7d7cafd1b5c336fd05a4e157422284e38229b53d
univac <68467947+univac-os@users.noreply.github.com>
2025-12-06 12:48:58 +0530

    Time: 7 ms (96.78%) | Memory: 18.6 MB (16.90%) - LeetSync

--- 126-word-ladder-ii/word-ladder-ii.py
+++ 126-word-ladder-ii/word-ladder-ii.py
@@ -4,59 +4,55 @@ class Solution:
         ties but its long method
         so wildcard pattern can be used here we want all the combination
         so bfs and create a graph and backtracking to get all 
+        we need shortest so who comes first out of queue it is shortest
         """
         if endWord not in wordList: return []
-        if beginWord==endWord:return [beginWord]
-        #1.create a pattern
-        pattern=defaultdict(list)
+        if beginWord==endWord :return [beginWord]
         wordList.append(beginWord)
         wordList=set(wordList)
+        #pattern
+        pattern=defaultdict(list)
+        n=len(beginWord)
         for w in wordList:
-            for i in range(len(w)):
+            for i in range(n):
                 pat=w[:i]+"*"+w[i+1:]
                 pattern[pat].append(w)
-
-        #2.BFS 
+        #BFS
         parent=defaultdict(list)
-        level=0
+        lev=0
+        lev_node={beginWord:0}
         q=deque([beginWord])
         found=False
-        curr_lev={beginWord:0}
         while q and not found:
-            level+=1
+            lev+=1
             for _ in range(len(q)):
-                word=q.popleft()
-                for i in range(len(word)):
-                    pat=word[:i]+"*"+word[i+1:]
+                w=q.popleft()
+                #check fist time see or on same level we have another option
+                for i in range(n):
+                    pat=w[:i]+"*"+w[i+1:]
                     for nei in pattern[pat]:
-                        if nei not in curr_lev:
-                            #new node
-                            curr_lev[nei]=level
-                            parent[nei].append(word) #parent of nei is word
+                        if nei not in lev_node:
+                            lev_node[nei]=lev
+                            parent[nei].append(w)
                             q.append(nei)
                         else:
-                            # nei in same level so we can get other shortest path
-                            if curr_lev[nei]==level:
-                                parent[nei].append(word)
-                                #no need to add to queue 
-
-                        if nei==endWord:
+                            if lev_node[nei]==lev:
+                                parent[nei].append(w)
+                    
+                        if endWord==nei:
                             found=True
-        #3. Backtracking 
+                
+        #backtracking
         res=[]
-        path=[endWord]
-
         def dfs(word,path):
-            if word == beginWord:
-                    res.append(path[::-1]) #begin-->end
-                    return 
+            if word==beginWord:
+                res.append(path[::-1])
+                return 
             
             for p in parent[word]:
                 path.append(p)
                 dfs(p,path)
                 path.pop()
-        
+
         dfs(endWord,[endWord])
         return res
-                        
-
